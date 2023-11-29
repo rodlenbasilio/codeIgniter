bf32d43e156cab9b36676fe6a5d60ff3ebab8c4f
Rodlen Basilio <44938452+rodlenbasilio@users.noreply.github.com>
2023-11-29 12:53:10 +0800

    .

---
+++ app/Controllers/Pages.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controllers;
+use CodeIgniter\Exceptions\PageNotFoundException;
+
+
+class Pages extends BaseController
+{
+    public function index()
+    {
+        return('welcome_message');
+    }
+
+
+    public function view($page = 'about')
+    {
+        //return('view ' . $page);
+       //return (APPPATH . 'Views/pages/' . $page . '.php');
+        if (! is_file(APPPATH . 'Views\\pages\\' . $page . '.php')) {
+            // Whoops, we don't have a page for that!
+            throw new PageNotFoundException($page);
+        }
+
+        $data['title'] = ucfirst($page); // Capitalize the first letter
+
+        return view('templates/header', $data)
+            . view('pages/' . $page)
+            . view('templates/footer');
+        
+    }
+        
+}
